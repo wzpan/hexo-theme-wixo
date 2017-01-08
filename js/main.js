349983ff8635b54995356b02a23d465550afd55b
Joseph Pan <m@hahack.com>
2017-01-08 16:20:16 +0000

    Site updated: 2017-01-08 16:20:14

--- js/main.js
+++ js/main.js
@@ -1,54 +1,41 @@
+function setTitleDate()
+{
+	var body_width = $("#body").css("width")
+	var container_width = $("#container").css("width")
+	var container_margin_right = $("#container").css("margin-right");
+	var container_padding_right = $("#container").css("padding-right");
+	//var container_right = parseInt(container_margin_right) + parseInt(container_padding_right);
+	var container_right = (parseInt(body_width) - parseInt(container_width))/2 + 10;
+	$("#title-date").css("right", container_right);
+}
+
 $(document).ready(function() {
+	
 	$(window).scroll(function(){  //只要窗口滚动,就触发下面代码 
         var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度 
-        if(scrollt>200){  //判断滚动后高度超过200px
+        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示
             $("#gotop").fadeIn(400); //淡出
-			if($(window).width() >= 1200){
-				$(".navbar").stop().fadeTo(400, 0.2);
-			}
+			$(".navbar").stop().fadeTo(400, 0.2);
         }else{
             $("#gotop").fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动
-            if($(window).width() >= 1200){
-				$(".navbar").stop().fadeTo(400, 1);
-            }
+			$(".navbar").stop().fadeTo(400, 1);
         }
     });
-    $("#gotop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部        
-		$("html,body").animate({scrollTop:"0px"},200);
+    $("#gotop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
+        $("html,body").animate({scrollTop:"0px"},200);
     });
 	$(".navbar").mouseenter(function(){
 		$(".navbar").fadeTo(100, 1);
 	});
     $(".navbar").mouseleave(function(){
 		var scrollt = document.documentElement.scrollTop + document.body.scrollTop;
-		if (scrollt>200) {
+		if ( scrollt > 200) {
 			$(".navbar").fadeTo(100, 0.2);
 		}
-	});
-
-	replaceMeta();
-
-	$(window).resize(function(){
-		replaceMeta();
-	});
+	});	
+	setTitleDate();
 });
 
-replaceMeta = function(){
-	if ($(window).width() < 980) {
-		if ($("#side_meta #post_meta").length>0) {
-			$("#post_meta").appendTo("#top_meta");
-		}
-		if ($("#sidebar #site_search").length>0) {
-			$("#site_search").appendTo("#top_search");
-			$("#site_search #st-search-input").css("width", "95%");
-		}
-	} else {
-		if ($("#top_meta #post_meta").length>0) {
-			$("#post_meta").appendTo("#side_meta");
-		}
-		if ($("#top_search #site_search").length>0) {
-			$("#site_search").prependTo("#sidebar");
-			$("#site_search #st-search-input").css("width", "85%");
-		}
-	}
-}
+$(window).resize(function () {
+	setTitleDate();
+})
